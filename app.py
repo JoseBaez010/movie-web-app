a597aa47735f22dfa75c030f72586232803b1df8
JoseBaez010 <josebaez0968@gmail.com>
2023-04-13 14:02:36 -0400

    Testing how it works

--- app.py
+++ app.py
@@ -8,45 +8,51 @@ background_color = "linear-gradient(180deg, #6E48AA, #B538FF)"
 
 st.markdown("<h1 style='text-align: center; color: black;'>Cinema Lit</h1>", unsafe_allow_html=True)
 
-
 api_key = "bdb0d72516db605476b9d810383f277e"
-url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={api_key}&language=en-US&page=1"
 
-# Fetch the list of top-rated movies from the TMDB API
-response = requests.get(url)
-data = response.json()
-movies = data["results"]
-df = pd.DataFrame(movies)
+option = st.sidebar.selectbox("Here are the other options: ", ("Main Page", "Movie Search", "Movie Statistics"))
 
-# Calculate the number of rows and columns needed for the grid
-num_cols = 5
-num_rows = (len(movies) + num_cols - 1) // num_cols
+if option == "Main Page":
+    # Fetch the list of top-rated movies from the TMDB API
+    url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={api_key}&language=en-US&page=1"
+    response = requests.get(url)
+    data = response.json()
+    movies = data["results"]
+    df = pd.DataFrame(movies)
 
-# Create a list of empty lists to hold the movie posters for each column
-poster_cols = [[] for _ in range(num_cols)]
+    # Calculate the number of rows and columns needed for the grid
+    num_cols = 5
+    num_rows = (len(movies) + num_cols - 1) // num_cols
 
-# Loop through the list of movies and add their posters to the appropriate column
-for i, movie in enumerate(movies):
-    # Retrieve the URL for the poster image of the movie
-    poster_path = movie["poster_path"]
-    poster_url = f"https://image.tmdb.org/t/p/w500/{poster_path}"
+    # Create a list of empty lists to hold the movie posters for each column
+    poster_cols = [[] for _ in range(num_cols)]
 
-    # Add the poster URL to the appropriate column
-    poster_cols[i % num_cols].append(poster_url)
+    # Loop through the list of movies and add their posters to the appropriate column
+    for i, movie in enumerate(movies):
+        # Retrieve the URL for the poster image of the movie
+        poster_path = movie["poster_path"]
+        poster_url = f"https://image.tmdb.org/t/p/w500/{poster_path}"
 
-# Display the posters in a grid of 5 columns and 5 rows
-for row_idx in range(num_rows):
-    # Create a list of columns for this row
-    row_cols = st.columns(num_cols)
+        # Add the poster URL to the appropriate column
+        poster_cols[i % num_cols].append(poster_url)
 
-    # Loop through the columns and display the posters
-    for col_idx in range(num_cols):
-        poster_idx = row_idx * num_cols + col_idx
-        if poster_idx < len(movies):
-            row_cols[col_idx].image(poster_cols[col_idx][row_idx],
-                                    caption=movies[poster_idx]["title"],
-                                    use_column_width=True)
+    # Display the posters in a grid of 5 columns and 5 rows
+    for row_idx in range(num_rows):
+        # Create a list of columns for this row
+        row_cols = st.columns(num_cols)
 
-# Display the data in a table
-table = st.dataframe(df)
+        # Loop through the columns and display the posters
+        for col_idx in range(num_cols):
+            poster_idx = row_idx * num_cols + col_idx
+            if poster_idx < len(movies):
+                row_cols[col_idx].image(poster_cols[col_idx][row_idx],
+                                        caption=movies[poster_idx]["title"],
+                                        use_column_width=True)
+
+elif option == "Movie Search":
+
+    st.title("Movie Search")
+    url = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}"
+    move_name = st.text_input("Enter the name of the movie you want to search?")
+    response = requests.get(url)
 
