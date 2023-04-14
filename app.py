893ee0dd9cbaa4f8a0024893d47da7228e0a1833
JoseBaez010 <josebaez0968@gmail.com>
2023-04-14 18:00:52 -0400

    Was able to get most of the features done just need to see how the map works with this

--- app.py
+++ app.py
@@ -11,45 +11,44 @@ api_key = "bdb0d72516db605476b9d810383f277e"
 option = st.sidebar.selectbox("Here are the other options: ", ("Main Page", "Movie Search", "Movie Statistics"))
 
 if option == "Main Page":
-    # Fetch the list of top-rated movies from the TMDB API
-    url = f"https://api.themoviedb.org/3/movie/top_rated?api_key={api_key}&language=en-US&page=1"
-    response = requests.get(url)
-    data = response.json()
-    movies = data["results"]
-    df = pd.DataFrame(movies)
-
-    # Calculate the number of rows and columns needed for the grid
-    num_cols = 5
-    num_rows = (len(movies) + num_cols - 1) // num_cols
-
-    # Create a list of empty lists to hold the movie posters for each column
-    poster_cols = [[] for _ in range(num_cols)]
-
-    # Loop through the list of movies and add their posters to the appropriate column
-    for i, movie in enumerate(movies):
-        # Retrieve the URL for the poster image of the movie
-        poster_path = movie["poster_path"]
-        poster_url = f"https://image.tmdb.org/t/p/w500/{poster_path}"
-
-        # Add the poster URL to the appropriate column
-        poster_cols[i % num_cols].append(poster_url)
-
-    # Display the posters in a grid of 5 columns and 5 rows
-    for row_idx in range(num_rows):
-        # Create a list of columns for this row
-        row_cols = st.columns(num_cols)
-
-        # Loop through the columns and display the posters
-        for col_idx in range(num_cols):
-            poster_idx = row_idx * num_cols + col_idx
-            if poster_idx < len(movies):
-                row_cols[col_idx].image(poster_cols[col_idx][row_idx],
-                                        caption=movies[poster_idx]["title"],
-                                        use_column_width=True)
+    st.title("Home Page")
 
 elif option == "Movie Search":
+    st.title("Movie Search App")
 
-    st.title("Movie Search")
-    url = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}"
-    move_name = st.text_input("Enter the name of the movie you want to search?")
-    response = requests.get(url)
+    # Define the search parameters
+    search_query = st.text_input("Enter movie title")
+    movie_genre = st.multiselect("Select genre",
+                                 ["Action", "Adventure", "Animation", "Comedy", "Crime", "Documentary", "Drama",
+                                  "Family", "Fantasy", "History", "Horror", "Music", "Mystery", "Romance",
+                                  "Science Fiction", "TV Movie", "Thriller", "War", "Western"])
+    year_range = st.slider("Select release year range", 1900, 2023, (1950, 2023))
+
+    # Make API request
+    if st.button("Search"):
+        url = f"https://api.themoviedb.org/3/search/movie?api_key={api_key}&query={search_query}"
+        response = requests.get(url).json()
+
+        # Filter movies based on release year range
+        filtered_movies = [movie for movie in response['results'] if
+                           year_range[0] <= int(movie['release_date'][:4]) <= year_range[1]]
+
+        # Filter movies based on genre(s)
+        if movie_genre:
+            genre_id_dict = {"Action": 28, "Adventure": 12, "Animation": 16, "Comedy": 35, "Crime": 80,
+                             "Documentary": 99,
+                             "Drama": 18, "Family": 10751, "Fantasy": 14, "History": 36, "Horror": 27, "Music": 10402,
+                             "Mystery": 9648, "Romance": 10749, "Science Fiction": 878, "TV Movie": 10770,
+                             "Thriller": 53, "War": 10752, "Western": 37}
+
+            filtered_movies = [movie for movie in filtered_movies if
+                               any(genre_id == genre_id_dict[genre] for genre_id in movie["genre_ids"] for genre in
+                                   movie_genre)]
+
+        # Display filtered movie results
+        for movie in filtered_movies:
+            st.write(movie["title"])
+            st.write(movie["overview"])
+            st.image(f"https://image.tmdb.org/t/p/w500{movie['poster_path']}")
+            st.write(f"Rating: {movie['vote_average']}")
+            st.write("---")
