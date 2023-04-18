fe476073ee9481e58466d2796210ee5e954a97f8
Jocelyn Rodriguez <jocelyndrodri@gmail.com>
2023-04-18 15:05:47 -0400

    Put the map with GoogleMaps trying to fix it

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 import streamlit as st
 import requests
 import pandas as pd
+import json
+import numpy as np
+import googlemaps
 
 background_color = "linear-gradient(180deg, #6E48AA, #B538FF)"
-
+# 5fa1e8 <- nice color to use as a theme
 st.markdown("<h1 style='text-align: center; color: black;'>Cinema Fanatic</h1>", unsafe_allow_html=True)
 st.divider()
 api_key = "bdb0d72516db605476b9d810383f277e"
@@ -12,7 +15,6 @@ option = st.sidebar.selectbox("Explore the web app: ", ("Home", "Search your Fav
 
 # Home Page
 if option == "Home":
-    #st.title("Home Page")
     st.header("Welcome to :blue[Cinema Fanatic], a place where movie enthusiasts get their crave on")
     st.divider()
     st.text("Cinema Fanatic aims to provide users access to data related to Top-Rated movies.\nOn top of that, users "
@@ -26,12 +28,48 @@ if option == "Home":
     data = response.json()
     movies = data["results"]
 
-    #create button to get recently released movies
+    # Create button to get recently released movies
     if st.button("Recently Released Movies"):
         table_data = []
         for movie in movies:
             table_data.append([movie["title"], movie["release_date"], movie["vote_average"]])
         st.table(table_data)
+
+    st.divider()
+
+
+    # Set up Google Maps API client
+    gmaps = googlemaps.Client(key='AIzaSyA0Ci-gyYa06Bswegp0Iub5ZiS0iVApK_U')
+
+    st.title("Find your Nearest Movie Theater")
+    zip_code = st.text_input("Enter your 5-digit zip code:")
+
+    # Check valid zipcode
+    if len(zip_code) != 5 or not zip_code.isdigit():
+        st.warning("Please enter a valid 5-digit zip code.")
+    else:
+        # Get lat and lng from zip code using geocoding API
+        response = gmaps.geocode(zip_code)
+        lat = response[0]['geometry']['location']['lat']
+        lng = response[0]['geometry']['location']['lng']
+
+        # Search for movie theaters near lat and lng using Places API
+        theaters = []
+        page_token = None
+        while True:
+            response = gmaps.places_nearby(location=(lat, lng), radius=50000, keyword='movie theater',
+                                           page_token=page_token)
+            theaters += response['results']
+            page_token = response.get('next_page_token')
+            if not page_token:
+                break
+
+        # Create dataframe of theater locations and plot on map
+        theaters_df = pd.DataFrame(
+            [(theater['geometry']['location']['lat'], theater['geometry']['location']['lng']) for theater in theaters],
+            columns=['lat', 'lon'])
+        st.map(theaters_df)
+
 
 # Search
 elif option == "Search your Favorites":
@@ -74,9 +112,12 @@ elif option == "Search your Favorites":
                 st.write(movie["overview"])
                 st.image(f"https://image.tmdb.org/t/p/w500{movie['poster_path']}")
                 st.write(f"Rating: {movie['vote_average']}")
-                st.write("---")
+                st.divider()
         else:
             st.error("Movies are not found here.")
+
+
+
 
 # Stats
 elif option == "Top-rated Data":
